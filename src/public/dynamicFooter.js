3a285ec3597957fc1884c7a4ae81263cceb0508f
harryvince <harryavince@gmail.com>
2021-11-17 20:04:25 +0000

    added external js

---
+++ src/public/dynamicFooter.js
@@ -0,0 +1,5 @@
+let ts = Date.now();
+let date_ob = new Date(ts);
+let year = date_ob.getFullYear();
+let footer = document.getElementById('DynamicFooter');
+footer.innerText = "Harry Vince - "+year; 
